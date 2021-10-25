01321201e6f09ff1265b751f2ebd82ef45a733b1
Adityadwika <adityadwika58@gmail.com>
2021-10-25 17:41:53 +0700

    my file

---
+++ shop/allpayment.js
@@ -0,0 +1,31 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY DITZ OFC
+
+*さ BANK*
+1. AXIS 
+	ㅁ 0838 2468 2542
+		• A***D*Y
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 0838 2468 2542
+		• DITZ STORE
+2. OVO
+	ㅁ 0838 2468 2542
+		• DITZ
+3. DANA
+	ㅁ 0838 2468 2542
+		• DITZ
+
+*さ PULSA*
+1. AXIS
+	ㅁ 0838 2468 2542
+2. TELKOMSEL
+	ㅁ 0821 2888 4955
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
